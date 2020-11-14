684cd002c949546136a2a3b724c10bae09e9d35c
jahiduloo7 <jahidul479@gmail.com>
2020-11-15 01:15:29 +0600

    completed

--- lib/widget/tasks_list.dart
+++ lib/widget/tasks_list.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:to_do_app/model/task.dart';
 import 'package:to_do_app/provider%20/task_data.dart';
 import 'package:to_do_app/widget/task_tile.dart';
 
